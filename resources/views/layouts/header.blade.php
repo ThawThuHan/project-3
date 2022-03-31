20cc25d80880de57d9075a89e2ef4897015719f4
ThawThuHan <thawthuhan9@gmail.com>
2022-03-31 22:17:55 +0630

    new home page design

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,35 @@
+<div class="container-fluid bg-light shadow-sm">
+       <div class="container pt-3">
+           <div class="d-flex flex-wrap justify-content-between align-items-center">
+               <img src="/images/logo.png" alt="" class="w-25">
+               <div>
+                    <button class="btn btn-dark btn-sm text-white">Subscribe</button>
+                    <button class="btn btn-outline-dark btn-sm">Login</button>
+               </div>
+           </div>
+       </div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="#">Entertainment</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Healthy</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Sport</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Beauty</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Technology</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Other</a></li>
+            </ul>
+            <form class="d-flex position-relative">
+                <input id="search-input" class="form-control rounded-pill" type="search" name="search"  placeholder="Search" aria-label="Search">
+                <button id="search-btn" type="submit" class="border-0 bg-white position-absolute top-50 end-0 translate-middle"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+            </div>
+        </div>
+    </nav>
+   </div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/all.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/master.css">
+    @yield('style')
+</head>
+<body>
+    @include('layouts.header')
+    @yield('content')
+
+    <script src="/js/bootstrap.bundle.js"></script>
+    <script src="/js/main.js"></script>
+    @yield('script')
+</body>
+</html>
